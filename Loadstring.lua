5a3217e777fc1f90a5faedd7baac2c7e3a951fc8
Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>
2023-02-13 18:15:33 -0800

    Update Loadstring.lua
    Signed-off-by: Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>

--- Loadstring.lua
+++ Loadstring.lua
@@ -6,7 +6,7 @@ Release = Status and Release
 local Script = not Release and readfile("rath/Init.lua") or game:HttpGet("https://raw.githubusercontent.com/frayray909090/testing/main/Init.lua")
 
 -- Import
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ro-Chat/Import/main/Main.lua"))()(Release, "Ro-Chat/rath", "main")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/frayray909090/penis/main/wtf.lua"))()(Release, "frayray909090/testing", "main")
 
 -- Main
 loadstring(Script)()
